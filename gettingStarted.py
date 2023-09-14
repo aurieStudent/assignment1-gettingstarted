dfb310dd8a4892a99d5042e0df53c89348a0022f
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2023-09-14 15:17:01 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -37,6 +37,7 @@ def welcome_assignment_answers(question):
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
+    answer="No"
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
