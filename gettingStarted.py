47c875e0d3459d56a8e96ea2df927e01a0fb1068
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2023-09-14 15:26:55 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,7 +6,6 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    answer="Yes"
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -39,7 +38,6 @@ if __name__ == "__main__":
     questions = ["Are encoding and encryption the same? - Yes/No","Is it possible to decrypt a message without a key? - Yes/No","In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?", "Is it possible to decode a message without a key - Yes/No", "Is a hashed message supposed to be un-hashed? - Yes/No", "What is the SHA256 hashing value of your NYU email and use the answer in your code -", "Is MD5 a secured hashing algorithm? - Yes/No", "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number", "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number" ]
 
     for debug_question in questions:
-    debug_question = "Are encoding and encryption the same? - Yes/No"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
     print(' ')
