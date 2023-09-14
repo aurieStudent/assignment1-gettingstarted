797628bb67beff63a0ab49c6d9defe685989adc6
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2023-09-14 14:46:44 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -11,7 +11,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "pcap"
+        answer = "'pcap'"
     elif question == "Is it possible to decode a message without a key - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
@@ -21,9 +21,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "1"
+        int(answer) = "1"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "2"
+        int(answer) = "2"
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
