f35ec97593819b348c9f98899b9c4fc26f75438c
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2023-09-14 15:25:31 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -36,9 +36,13 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
+    questions = ["Are encoding and encryption the same? - Yes/No","Is it possible to decrypt a message without a key? - Yes/No","In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?", "Is it possible to decode a message without a key - Yes/No", "Is a hashed message supposed to be un-hashed? - Yes/No", "What is the SHA256 hashing value of your NYU email and use the answer in your code -", "Is MD5 a secured hashing algorithm? - Yes/No", "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number", "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number" ]
+
+    for debug_question in questions:
     debug_question = "Are encoding and encryption the same? - Yes/No"
-    answer="No"
+    print(debug_question)
     print(welcome_assignment_answers(debug_question))
+    print(' ')
 
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
